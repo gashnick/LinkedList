669705fc2dd4dc93e0de86bd922b94b3321b90b8
gashnick <88553941+gashnick@users.noreply.github.com>
2022-11-11 17:56:10 +0200

    Add files via upload

---
+++ TestMyLinkedList.java
@@ -0,0 +1,40 @@
+
+import mylinkedlist.LinkedList1;
+
+
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+         
+        LinkedList1 l1  =  new LinkedList1();
+        LinkedList1 l2  =  new LinkedList1();
+        LinkedList1 s  =  new LinkedList1();
+
+        l1.insertAtBack(2);
+        l1.insertAtBack(7);
+        l1.insertAtBack(12);
+
+        l2.insertAtBack(13);
+        l2.insertAtBack(94);
+        l2.insertAtBack(42);
+        
+        System.out.println("Elements of linkedlist 1 are: "+l1);
+        System.out.println("Elements of linkedlist 2 are: "+l2);
+  
+
+        
+        
+        System.out.println(l1.isSorted(l2));
+        
+       
+        
+        // concatinate two linkedlists
+        
+        System.out.println("Concatineted linkedlist is: "+l1.concantenate(l2));
+        
+        // Merging two linkedlists
+      
+        System.out.print("Two merged linkedlists are: ");  
+        l1.merge(l2);
+    }
+}
